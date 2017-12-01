97ab6527e80e40b6e28ab7bd2a1fb77471934e5c
ForEntropy <walterjoshuabenjamin@gmail.com>
2017-11-30 21:43:42 -0800

    Update main.cpp
    Added the Combat loop as an extern for testing.
    This version works though.

--- main.cpp
+++ main.cpp
@@ -3,55 +3,64 @@
 #include "Potion.h"
 using namespace std;
 
+extern void Combat(Character player1, Character enemy1, MagicMissile missile, HealthPotion pot1, Sword sword1);
+
 int main() {
 
-    Character player1("Mad Max", 100);      // Create a character
-    Character enemy1("The Toecutter", 100); // Create a character
-    Sword sword1("Master Sword", 3);        // Create a Sword (weapon)
-    HealthPotion pot1(2);                   // Create a HealthPotion
-    StrengthPotion pot2(3);                 // Create a StrengthPotion
-    HealingSpell hSpell(1);                 // A healing spell
-    MagicMissile missile(3);                // Magic Missile spell
+	Character player1("Mad Max", 100);      // Create a character
+	Character enemy1("The Toecutter", 100); // Create a character
+	Sword sword1("Master Sword", 3);        // Create a Sword (weapon)
+	HealthPotion pot1(2);                   // Create a HealthPotion
+	StrengthPotion pot2(3);                 // Create a StrengthPotion
+	HealingSpell hSpell(1);                 // A healing spell
+	MagicMissile missile(3);                // Magic Missile spell
 
 
-    // Demonstration of combat between 2 Characters
-    cout << player1.GetName() << " enters the battle!" << endl;
-    cout << enemy1.GetName() << " enters the battle!" << endl;
+											// Demonstration of combat between 2 Characters
 
-    player1.Print();    // Display player1 attributes
-    enemy1.Print();     // Display enemy1 attributes
+	Combat(player1, enemy1, missile, pot1, sword1);
 
-    // Player attacks enemy
-    player1.Attack(&enemy1);
-    enemy1.Attack(&player1);
-    cout << endl;
+	/*
+	cout << player1.GetName() << " enters the battle!" << endl;
+	cout << enemy1.GetName() << " enters the battle!" << endl;
 
-    // Test use of strength potion
-    player1.UseConsumable(&pot2);   // Increase player1 strength
-    cout << endl;
+	player1.Print();    // Display player1 attributes
+	enemy1.Print();     // Display enemy1 attributes
 
-    // Player attacks enemy
-    player1.Attack(&enemy1);
-    cout << endl;
+						// Player attacks enemy
+	player1.Attack(&enemy1);
+	enemy1.Attack(&player1);
+	cout << endl;
 
-    // Test use of healing spell
-    player1.CastSpell(&hSpell, &enemy1);
-    cout << endl;
+	// Test use of strength potion
+	player1.UseConsumable(&pot2);   // Increase player1 strength
+	cout << endl;
 
-    // Test use of magic missile spell
-    player1.CastSpell(&missile, &enemy1);
-    cout << endl;
+	// Player attacks enemy
+	player1.Attack(&enemy1);
+	cout << endl;
 
-    // Enemy attacks player
-    enemy1.Attack(&player1);
-    cout << endl;
+	// Test use of healing spell
+	player1.CastSpell(&hSpell, &enemy1);
+	cout << endl;
 
-    // Test use of health potion
-    player1.UseConsumable(&pot1);   // Replenish player1 health
-    cout << endl;
+	// Test use of magic missile spell
+	player1.CastSpell(&missile, &enemy1);
+	cout << endl;
 
-    player1.Print();
-    enemy1.Print();
+	// Enemy attacks player
+	enemy1.Attack(&player1);
+	cout << endl;
 
-    return 0;
+	// Test use of health potion
+	player1.UseConsumable(&pot1);   // Replenish player1 health
+	cout << endl;
+
+	player1.Print();
+	enemy1.Print();
+	*/
+	
+	// Added Pause to look at output -J
+	system("PAUSE");
+	return 0;
 }
