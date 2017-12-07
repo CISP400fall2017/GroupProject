c5d4e1ec4377d992171e58dbec850a1536445f28
ForEntropy <walterjoshuabenjamin@gmail.com>
2017-12-06 16:59:23 -0800

    Create Combat.h
    Added Combat header

---
+++ Combat.h
@@ -0,0 +1,21 @@
+#pragma once
+/*******************************************************************************
+*              Base class for all consumable items
+*******************************************************************************/
+#ifndef GROUPPROJECT_CONSUMABLE_H
+#define GROUPPROJECT_CONSUMABLE_H
+
+#include "Item.h"
+
+class Character;
+class Consumable : public Item {
+protected:
+	int quantity;       // Quantity held by player
+public:
+	Consumable();
+	int GetQuantity() const;
+	void SetQuantity(int quantity);
+	virtual void Consume(Character *target) = 0;
+};
+
+#endif //GROUPPROJECT_CONSUMABLE_H
